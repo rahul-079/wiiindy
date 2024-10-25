8e4bee3f5e32415f5b06f88c959a6a5a7f94466c
rahul-079 <121021984+rahul-079@users.noreply.github.com>
2024-10-25 12:19:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function dateManage(dateArg) {
     let month=months[dateArg.getMonth()];
     let date=dateArg.getDate();
     let day=days[dateArg.getDay()];
-    return `${date}${month}(${day}),${year}`;
+    return `${date} ${month}(${day}),${year}`;
 }
 
 
